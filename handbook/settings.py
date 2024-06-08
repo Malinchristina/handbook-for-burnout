b266d87d526030400867a498e2beadc8d8c08598
Malinchristina <malin_christina@yahoo.se>
2024-06-08 21:40:08 +0200

    Add model for category app to add category, set deploy settings

--- handbook/settings.py
+++ handbook/settings.py
@@ -89,6 +89,11 @@ DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+CSRF_TRUSTED_ORIGINS = [
+    "https//*.herokuapp.com"
+    "http://*127.0.0.1:8000/"
+]
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
