51e1f117b7e3f9054b13ce5b190cf20911bae04f
Malinchristina <malin_christina@yahoo.se>
2024-06-09 20:20:31 +0200

    Register AddCategory model in admin panel

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import AddCategory
 
 # Register your models here.
+
+@admin.register(AddCategory)
+class AddCategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_id', 'category_name', 'author', 'created_on')
+    search_fields = ['category_name']
+    readonly_fields = ('category_id', 'created_on')
--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class AddCategory(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.category_name
