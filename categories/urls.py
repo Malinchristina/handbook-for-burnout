f06c2e74c6d888258ed530372a79c03ea739a667
Malinchristina <malin_christina@yahoo.se>
2024-07-23 22:18:15 +0200

    set up html pages, views and urls for different categories

--- categories/urls.py
+++ categories/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.category_list, name='categories'),
+    path('routines/', views.routines_view, name='routines'),
+    path('podcasts/', views.podcasts_view, name='podcasts'),
+    path('indoor-activities/', views.indoor_activities_view, name='indoor-activities'),
+    path('outdoor-activities/', views.outdoor_activities_view, name='outdoor-activities'),
     path('addcategory/', views.add_category, name='add_category'),
 ]
