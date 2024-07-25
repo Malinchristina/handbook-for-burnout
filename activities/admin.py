708d409f8ccf96501e2da1e65050b6a1d06f1d92
Malinchristina <malin_christina@yahoo.se>
2024-07-25 11:29:49 +0200

    create model AddActivity, register it and migrate

--- activities/admin.py
+++ activities/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.http import HttpResponseRedirect
+from .models import AddActivity
 
 # Register your models here.
+
+@admin.register(AddActivity)
+class AddActivity(admin.ModelAdmin):
+    list_display = ('activity_id', 'activity_name', 'category', 'author', 'created_on')
+    search_fields = ['activity_name']
+    readonly_fields = ('activity_id', 'created_on')
