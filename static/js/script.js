b160bf2866cf5bb4155303a7721b1e35646017e9
Malinchristina <malin_christina@yahoo.se>
2024-07-26 14:41:47 +0200

    remove js

--- static/js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('myForm');
-    form.addEventListener('submit', function(event) {
-        event.preventDefault();
-        // Process form submission here
-        form.reset(); // Reset the form fields
-    });
-});
