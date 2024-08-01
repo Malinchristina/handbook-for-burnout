1747083368d52a0abe2601165b41556459630bee
Malinchristina <malin_christina@yahoo.se>
2024-08-01 13:24:05 +0200

    collect statics

--- staticfiles/js/reviews.js
+++ staticfiles/js/reviews.js
@@ -1,3 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var deleteModal = document.getElementById('deleteModal');
+    var deleteForm = document.getElementById('deleteForm');
+
+    deleteModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget;
+        var deleteUrl = button.getAttribute('data-delete-url');
+        deleteForm.setAttribute('action', deleteUrl);
+    });
+});
+
 function ToEditReview(button) {
     const url = button.getAttribute('data-edit-url');
     window.location.href = url;
