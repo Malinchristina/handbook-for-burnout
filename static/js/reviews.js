80c7665566be07fd041195fe06388408f9b2c278
Malinchristina <malin_christina@yahoo.se>
2024-08-12 17:55:06 +0200

    comment and clean up code

--- static/js/reviews.js
+++ static/js/reviews.js
@@ -1,15 +1,15 @@
+// Wait for the DOM to fully load before executing the script
+
 document.addEventListener('DOMContentLoaded', function () {
     var deleteModal = document.getElementById('deleteModal');
     var deleteForm = document.getElementById('deleteForm');
 
+    // Show the delete modal when the delete button is clicked
     deleteModal.addEventListener('show.bs.modal', function (event) {
         var button = event.relatedTarget;
         var deleteUrl = button.getAttribute('data-delete-url');
+
+        // Set the action of the delete form to the delete URL
         deleteForm.setAttribute('action', deleteUrl);
     });
 });
-
-function ToEditReview(button) {
-    const url = button.getAttribute('data-edit-url');
-    window.location.href = url;
-}
