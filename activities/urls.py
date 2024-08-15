b69f624962164522e6c39b1a7c95c701193dc5f7
Malinchristina <malin_christina@yahoo.se>
2024-08-15 20:19:10 +0200

    set upp mixin views for secure CRUD

--- activities/urls.py
+++ activities/urls.py
@@ -1,16 +1,25 @@
 from django.urls import path
-from . import views
+from .views import (
+     ActivitiesView,
+     AddActivityView,
+     EditActivityView,
+     DeleteActivityView,
+     routines_view,
+     podcasts_view,
+     indoor_activities_view,
+     outdoor_activities_view,
+)
+
 
 urlpatterns = [
-    path('addactivity/', views.add_activity, name='add_activity'),
-    path('routines/', views.routines_view, name='routines'),
-    path('podcasts/', views.podcasts_view, name='podcasts'),
-    path('indoor-activities/', views.indoor_activities_view,
-         name='indoor-activities'),
-    path('outdoor-activities/',
-         views.outdoor_activities_view, name='outdoor-activities'),
-    path('editactivity/<int:pk>/',
-         views.edit_activity, name='edit_activity'),
-    path('deleteactivity/<int:pk>/',
-         views.delete_activity, name='delete_activity'),
+     path('', ActivitiesView.as_view(), name='activities'),
+     path('add_activity/', AddActivityView.as_view(), name='add_activity'),
+     path('edit_activity/<int:pk>/', EditActivityView.as_view(), name='edit_activity'),
+     path('delete_activity/<int:pk>/', DeleteActivityView.as_view(), name='delete_activity'),
+     path('routines/', routines_view, name='routines'),
+     path('podcasts/', podcasts_view, name='podcasts'),
+     path('indoor_activities/', indoor_activities_view, name='indoor_activities'),
+     path('outdoor_activities/', outdoor_activities_view, name='outdoor_activities'),
 ]
+
+
--- activities/views.py
+++ activities/views.py
@@ -1,6 +1,8 @@
 from django.db.models import Count
 from django.shortcuts import render, redirect, get_object_or_404, reverse
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from .forms import AddActivityForm
@@ -8,40 +10,72 @@ from .models import AddActivity
 from categories.models import AddCategory
 
 
-@login_required
-@user_passes_test(lambda u: u.is_staff)
-def add_activity(request):
-    if request.method == 'POST':
-        form = AddActivityForm(request.POST)
-        if form.is_valid():
-            activity = form.save(commit=False)
-            activity.author = request.user
-            activity.save()
-            messages.success(request, 'The activity was successfully added.')
-            return redirect('categories')
-    else:
-        form = AddActivityForm()
-    return render(request, 'activities/add_activity.html', {'form': form})
+def is_staff_or_superuser(user):
+    return user.is_staff or user.is_superuser
 
 
-def edit_activity(request, pk):
-    activity = get_object_or_404(AddActivity, pk=pk)
-    if request.method == 'POST':
-        form = AddActivityForm(request.POST, instance=activity)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'The activity was successfully updated.')
-            return redirect('categories')
-    else:
-        form = AddActivityForm(instance=activity)
-    return render(request, 'activities/edit_activity.html', {'form': form})
+def not_staff_or_superuser(request):
+    return render(request, '403.html')
 
 
-def delete_activity(request, pk):
-    activity = get_object_or_404(AddActivity, pk=pk)
-    activity.delete()
-    messages.success(request, 'The activity was successfully deleted.')
-    return redirect('categories')
+class ActivitiesView(ListView):
+    model = AddActivity
+    template_name = 'activities/activities.html'
+    context_object_name = 'activities'
+    ordering = ['-date_posted']
+    paginate_by = 5
+
+
+class AddActivityView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
+    model = AddActivity
+    form_class = AddActivityForm
+    template_name = 'activities/add_activity.html'
+    success_url = reverse_lazy('activities:activities')
+
+    def test_func(self):
+        return is_staff_or_superuser(self.request.user)
+
+    def handle_no_permission(self):
+        return not_staff_or_superuser(self.request)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, 'Activity Added Successfully')
+        return super().form_valid(form)
+
+    
+class EditActivityView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = AddActivity
+    form_class = AddActivityForm
+    template_name = 'activities/edit_activity.html'
+    success_url = reverse_lazy('activities:activities')
+
+    def test_func(self):
+        return is_staff_or_superuser(self.request.user)
+
+    def handle_no_permission(self):
+        return not_staff_or_superuser(self.request)
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, 'Activity Updated Successfully')
+        return super().form_valid(form)
+
+
+class DeleteActivityView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = AddActivity
+    template_name = 'activities/delete_activity.html'
+    success_url = reverse_lazy('activities:activities')
+
+    def test_func(self):
+        return is_staff_or_superuser(self.request.user)
+
+    def handle_no_permission(self):
+        return not_staff_or_superuser(self.request)
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, 'Activity Deleted Successfully')
+        return super().delete(request, *args, **kwargs)
 
 
 def routines_view(request):
@@ -57,7 +91,8 @@ def routines_view(request):
 
 def podcasts_view(request):
     category = get_object_or_404(AddCategory, category_name='Podcasts')
-    activities = AddActivity.objects.filter(category=category)
+    activities = AddActivity.objects.filter(
+        category=category).annotate(review_count=Count('reviews'))
     return render(request, 'activities/podcasts.html', {
         'activities': activities,
         'category_name': category.category_name,
@@ -68,7 +103,8 @@ def podcasts_view(request):
 def indoor_activities_view(request):
     category = get_object_or_404(
         AddCategory, category_name='Indoor activities')
-    activities = AddActivity.objects.filter(category=category)
+    activities = AddActivity.objects.filter(
+        category=category).annotate(review_count=Count('reviews'))
     return render(request, 'activities/indoor_activities.html', {
         'activities': activities,
         'category_name': category.category_name,
@@ -79,7 +115,8 @@ def indoor_activities_view(request):
 def outdoor_activities_view(request):
     category = get_object_or_404(
         AddCategory, category_name='Outdoor activities')
-    activities = AddActivity.objects.filter(category=category)
+    activities = AddActivity.objects.filter(
+        category=category).annotate(review_count=Count('reviews'))
     return render(request, 'activities/outdoor_activities.html', {
         'activities': activities,
         'category_name': category.category_name,
