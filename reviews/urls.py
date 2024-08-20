6655225346c3eefab8e75fd7c619f85a28d1fc86
Malinchristina <malin_christina@yahoo.se>
2024-08-20 16:23:51 +0200

    fix EditReveiw to show sucess message

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from .views import (
      ReviewsView,
      AddReview,
+     EditReview,
 
 )
 
@@ -9,7 +10,9 @@ from .views import (
 urlpatterns = [
      path('activity/<int:pk>/reviews/', ReviewsView.as_view(),
           name='activity_reviews'),
-    path('activity/<int:pk>/add_review/', AddReview.as_view(), 
-         name='add_review'),
+     path('activity/<int:pk>/add_review/', AddReview.as_view(), 
+          name='add_review'),
+     path('activity/<int:activity_pk>/edit_review/<int:pk>/',
+          EditReview.as_view(), name='edit_review'),
     
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -65,5 +65,62 @@ class AddReview(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         activity_pk = self.kwargs.get('pk')
         activity = get_object_or_404(AddActivity, pk=activity_pk)
         context['activity_name'] = activity.activity_name
+        context['activity_pk'] = activity_pk
         context['activity'] = activity
         return context
+
+    
+# class EditReview(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+#     model = Review
+#     form_class = ReviewForm
+#     template_name = 'reviews/edit_reviews.html'
+
+#     def test_func(self):
+#         return self.request.user.is_authenticated
+
+#     def form_valid(self, form):
+#         form.instance.author = self.request.user
+#         messages.success(self.request, 'Review Updated Successfully')
+#         return super().form_valid(form)
+
+#     def get_success_url(self):
+#         return reverse_lazy('activity_reviews', kwargs={'pk': self.kwargs['pk']})
+
+#     def get_context_data(self, **kwargs):
+#         context = super().get_context_data(**kwargs)
+#         activity_pk = self.kwargs.get('pk')
+#         activity = get_object_or_404(AddActivity, pk=activity_pk)
+#         context['activity_name'] = activity.activity_name
+#         context['activity_pk'] = activity_pk
+#         context['activity'] = activity
+#         context['form'] = self.get_form()
+#         return context
+
+
+class EditReview(UserPassesTestMixin, UpdateView):
+    model = Review
+    form_class = ReviewForm
+    template_name = 'reviews/edit_reviews.html'
+
+    def get_success_url(self):
+        return reverse_lazy('activity_reviews', kwargs={
+            'pk': self.object.activity_pk.pk})
+
+    def test_func(self):
+        review = self.get_object()
+        return self.request.user == review.author
+
+    def form_valid(self, form):
+        self.object = form.save()
+        messages.success(
+        self.request, 'Your review was successfully updated.')
+        return redirect(self.get_success_url())
+
+    def form_invalid(self, form):
+        messages.error(self.request,
+        'There was an error updating your review.')
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def handle_no_permission(self):
+        return redirect('not_authorized')
+    
