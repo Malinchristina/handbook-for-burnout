a1007809b91a1ac1c8c8a128be0edcb830d7234a
Malinchristina <malin_christina@yahoo.se>
2024-08-20 15:46:26 +0200

    get reveiw list and add review to work.

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,19 +1,15 @@
 from django.urls import path
 from .views import (
-    add_review, 
-    ReviewsView,
-    EditReview,
-    DeleteReview,
+     ReviewsView,
+     AddReview,
+
 )
 
 
 urlpatterns = [
-    path('add_review/<int:activity_pk>/', add_review,
+     path('activity/<int:pk>/reviews/', ReviewsView.as_view(),
+          name='activity_reviews'),
+    path('activity/<int:pk>/add_review/', AddReview.as_view(), 
          name='add_review'),
-    path('edit_review/<int:pk>/', EditReview.as_view(),
-         name='edit_review'),
-    path('delete_review/<int:pk>/', DeleteReview.as_view(),
-         name='delete_review'),
-    path('activity/<int:pk>/reviews/', ReviewsView.as_view(), 
-         name='activity_reviews'),
+    
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,51 +1,23 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
-from django.views.generic.edit import UpdateView
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.urls import reverse_lazy
-from django.views.generic import (ListView, DetailView,
-                                  CreateView, UpdateView, DeleteView)
-from .models import Review
-from .forms import ReviewForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (ListView, DetailView, CreateView,
+                                  UpdateView, DeleteView)
 from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .forms import ReviewForm
+from .models import Review
 from activities.models import AddActivity
-from django.http import HttpResponse, HttpResponseForbidden
 
 
 # Create your views here.
 
-@login_required
-def add_review(request, activity_pk):
-    activity = get_object_or_404(AddActivity, pk=activity_pk)
-    activity_name = activity.activity_name
-
-    if request.method == 'POST':
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            comment = form.cleaned_data['comment']
-            author = request.user
-
-            review = Review.objects.create(
-                comment=comment,
-                activity_pk=activity,
-                author=author
-            )
-
-            review.save()
-            messages.success(request, 'Your review was successfully added.')
-            return redirect('activity_reviews', pk=activity_pk)
-
-    form = ReviewForm()
-
-    all_reviews = Review.objects.filter(activity_pk=activity)
-    return render(request, 'reviews/add_review.html', {
-        'form': form,
-        'activity_name': activity_name,
-        'reviews': all_reviews
-    })
+def handle_no_permission(request):
+    return render(request, '403.html')
 
 
-class ReviewsView(ListView):
+class ReviewsView(LoginRequiredMixin, ListView):
     model = Review
     template_name = 'reviews/reviews.html'
     context_object_name = 'reviews'
@@ -67,71 +39,31 @@ class ReviewsView(ListView):
         context['form'] = form
         return context
 
-    def post(self, request, *args, **kwargs):
-        activity_pk = request.POST.get('activity_pk')
-        activity = get_object_or_404(AddActivity, pk=activity_pk)
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            review = form.save(commit=False)
-            review.activity_pk = activity
-            review.author = request.user
-            review.save()
-            return redirect('activity_reviews', pk=activity.pk)
-        else:
-            return self.get(request, *args, **kwargs)
-
-
-def not_authorized(request):
-    return render(request, '403.html')
-
-
-class EditReview(UserPassesTestMixin, UpdateView):
+    
+class AddReview(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Review
     form_class = ReviewForm
-    template_name = 'reviews/edit_reviews.html'
-
-    def get_success_url(self):
-        return reverse_lazy('activity_reviews', kwargs={
-            'pk': self.object.activity_pk.pk})
+    template_name = 'reviews/reviews.html'
 
     def test_func(self):
-        review = self.get_object()
-        return self.request.user == review.author
+        return self.request.user.is_authenticated
 
     def form_valid(self, form):
-        self.object = form.save()
-        messages.success(
-            self.request, 'Your review was successfully updated.')
-        return redirect(self.get_success_url())
-
-    def form_invalid(self, form):
-        messages.error(self.request,
-                       'There was an error updating your review.')
-        return self.render_to_response(self.get_context_data(form=form))
-
-    def handle_no_permission(self):
-        return redirect('not_authorized')
-
-
-class DeleteReview(UserPassesTestMixin, DeleteView):
-    model = Review
-
-    def test_func(self):
-        review = self.get_object()
-        return (
-            self.request.user == review.author or
-            self.request.user.is_staff or
-            self.request.user.is_superuser
-        )
+        activity_pk = self.kwargs.get('pk')
+        activity = get_object_or_404(AddActivity, pk=activity_pk)
+        form.instance.author = self.request.user
+        form.instance.activity_pk = activity
+        response = super().form_valid(form)
+        messages.success(self.request, 'Review Added Successfully')
+        return super().form_valid(form)
 
     def get_success_url(self):
-        return reverse_lazy('activity_reviews', kwargs={
-            'pk': self.object.activity_pk.pk})
+        return reverse_lazy('activity_reviews', kwargs={'pk': self.kwargs['pk']})
 
-    def handle_no_permission(self):
-        return redirect('not_authorized')
-
-    def delete(self, request, *args, **kwargs):
-        messages.success(
-            self.request, 'Your review was successfully deleted.')
-        return super().delete(request, *args, **kwargs)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        activity_pk = self.kwargs.get('pk')
+        activity = get_object_or_404(AddActivity, pk=activity_pk)
+        context['activity_name'] = activity.activity_name
+        context['activity'] = activity
+        return context
